99645c1d0b6e826e51d09e3ac68f3e73599851ec
Екатерина Воробьевская <113284505+vorobevskaya@users.noreply.github.com>
2023-04-13 22:39:05 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import mannwhitneyu
 
+chat_id = 689606612 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    p = mannwhitneyu(x, y).pvalue
+    if p >= 0.09:
+      result = bool(0)
+    else:
+      result = bool(1)
+    return result # Ваш ответ, True или False
